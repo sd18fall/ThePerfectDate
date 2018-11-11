f2733eea45a65dd87259165ed8bf4a81716e832b
Sabrina Pereira <sabrina.pereira@students.olin.edu>
2018-11-11 15:23:50 -0500

    Submitting code for code review

--- game.py
+++ game.py
@@ -1,18 +1,17 @@
 """
 MetaGame
-An interactive video cd ~
-game!
+An interactive video game!
 
 Authors: Cynthia Yong, Sabrina Pereira, Sophie Schaffer
 """
 import pygame
-from Data_Storage import*
 
 pygame.init()
 
 
 
-#Setting the standars sizes for the text boxes and buttons
+#Setting the standard sizes for the text boxes and buttons
+textFont = pygame.font.SysFont('comicsans', 30)
 width = 800
 height = 600
 buttonWidth = 200
@@ -20,16 +19,20 @@ buttonHeight = 100
 buttonY = 400
 textBoxWidth = 400
 textBoxHeight = 200
-textBoxY = 100
+textBoxY = 125
 textBoxX = (width - textBoxWidth)/2
 win = pygame.display.set_mode((width,height))
-pic = pygame.image.load("Creepy.jpg")
+pic = pygame.image.load("Creepy.jpg") #the background image for our game - there will eventually be different backgrounds as the player progresses
 
 
 
 
 class TextBox():
-    """needs to accept dimensions, color, text, location """
+    """
+    This is used to display text on the screen.
+    Requires textbox dimensions, color, text, location
+
+    """
 
     def __init__(self, color, x=textBoxX,y=textBoxY,width=textBoxWidth,height = textBoxHeight, text='', exist = False):
         self.color = color
@@ -39,9 +42,33 @@ class TextBox():
         self.height = height
         self.text = text
         self.exist = exist
+        self.dimensions = (width,height)
+
+    def textSpacing(self, font = textFont, color=pygame.Color('black')):
+        """
+        Method able to adjust the lines of text to fit in text box.
+
+        * Not currently in use
+        """
+        print(type(self.text))
+        words = [word.split(' ') for word in self.text.splitlines()]  # 2D array where each row is a list of words.
+        space = font.size(' ')[0]  # The width of a space.
+        max_width, max_height = self.dimensions
+        x, y = self.x+10,self.y+10
+        for line in words:
+            for word in line:
+                word_surface = font.render(word, 0, color)
+                word_width, word_height = word_surface.get_size()
+                if x + word_width >= max_width:
+                    x = pos[0]  # Reset the x.
+                    y += word_height  # Start on new row.
+                self.blit(word_surface, (x, y))
+                x += word_width + space
+            x = pos[0]  # Reset the x.
+            y += word_height  # Start on new row.
 
     def draw(self,win,outline=None):
-        #Call tButtonshis method to draw the button on the screen
+        #Call this method to draw the button on the screen
         if outline:
             pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
 
@@ -49,9 +76,14 @@ class TextBox():
 
         if self.text != '':
             #Set font and size of button text, display centered
-            font = pygame.font.SysFont('comicsans', 30)
+            font = textFont
             text = font.render(self.text, 1, (0,0,0))
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+        # if self.text != '':
+        #     self.textSpacing(self)
+
+
 
     def __str__(self):
         return self.text
@@ -60,11 +92,14 @@ class TextBox():
         return self.text
 
 class Button(TextBox):
-    """Child to text box,
-    on screen and where it leads"""
+    """
+    This class inherits from the textbox class to display the buttons and
+    allow user interactivity
+
+    """
 
     def __init__(self, color, x=0,y=buttonY,width=buttonWidth,height=buttonHeight, text='', exist = False):
-        # super().__init__(color, x)
+        # super().__init__(color, x) # we eventually want to use this method instead of writing everything out
         self.color = color
         self.y = buttonY
         self.width = buttonWidth
@@ -73,13 +108,14 @@ class Button(TextBox):
         self.text = text
 
     def isOver(self,pos):
-        #Pos is the mouse position or a tuple of (x,y) coordinates
+        # Pos is the mouse position or a tuple of (x,y) coordinates
             if pos[0] > self.x and pos[0] < self.x + self.width:
                 if pos[1] > self.y and pos[1] < self.y + self.height:
                     return True
             return False
 
     def isClick(self,pos):
+        # Will recognize when the user pushes down on the mouse
         if self.exist:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.isOver(pos):
@@ -88,7 +124,13 @@ class Button(TextBox):
                 return False
 
 class BackButton(Button):
-    def __init__(self, color, x=600,y=50,width=150,height=100, text='', exist = False):
+    """
+    This button allows the user to go back to the previous page
+    It has it's own class because it remains static in the corner and
+    doesn't need positioning
+
+    """
+    def __init__(self, color, x=625,y= 25,width=150,height=100, text='', exist = False):
         # super().__init__(color, x)
         self.color = color
         self.y = y
@@ -101,7 +143,11 @@ class BackButton(Button):
 
 
 class State():
-    """stores all the information of what has already happened"""
+    """
+    Stores all the information of what has already happened
+
+    * Not currently in use
+    """
     def __init__(self, level, inventory, location):
         self.level = level
         self.inventory = inventory
@@ -112,124 +158,158 @@ class State():
 
 
 class Room():
-    """needs to accept name, picture, artifacts"""
-    def __init__(self, name, picture, artifacts):
+    """
+    Stores the information for room
+
+    * Not currently in use
+    """
+    def __init__(self, name, picture):
         self.name = name
         self.picture = picture
-        self.artifacts = artifacts
 
 class Artifact():
-    """needs to accept name, description, picture"""
+    """
+    Stores information about the items that the user interacts with to help them
+
+    * Not currently in use
+    """
     def __init__(self, location, name, description, picture):
         self.name = name
         self.description = description
         self.picture = picture
 
 def Quitting():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-           run = False
-           pygame.quit()
-           quit()
+    """
+    Enables user to click to exit the game
+    """
+    if event.type == pygame.QUIT:
+       run = False
+       pygame.quit()
+       quit()
 
-def newScreen(dictkey,oldScreen = []):
-    """clears last display and puts up what should currently be on the screen"""
-    win.blit(pygame.transform.scale(pic, (width, height)), (0, 0))
+def newScreen(dictkey,dictionary,oldScreen = []):
+    """
+    This function clears the last display and
+    puts up a new screen after the user selects an option
+
+    """
+    win.blit(pygame.transform.scale(pic, (width, height)), (0, 0)) #photo background for screens
     for item in oldScreen:
-        item.exist = False
-    newItems = Screens[dictkey]
+        item.exist = False # eliminates the buttons in the previous screen
+    newItems = dictionary[dictkey] # gathers values (textboxes & buttons) based on dictionary key
     buttonList = []
     for item in newItems:
         if type(item) == Button:
-            buttonList.append(item)
-    buttonPlacement(buttonList)
-    for item in newItems:
+            buttonList.append(item) # putting the buttons into a separate list
+    buttonPlacement(buttonList) # spaces the buttons evenly on the screen
+    for item in newItems: #draws the textboxes and buttons
         item.draw(win,(0,0,0))
         item.exist = True
     return newItems
 
-def isAnythingClick(screenButtons):
+def isAnythingClick(screenButtons,dictionary):
+    """
+    This function detects if any of the buttons are clicked,
+    if a button is clicked, then it will return a new screen
+
+    """
     for item in screenButtons:
-        if type(item) == Button:
-            if item.isClick(pos):
-                newButtons = newScreen(item,screenButtons)
-                return newButtons
+        if type(item) == Button or type(item) == BackButton:
+            if item.isOver(pos):
+                item.draw(win,(255,0,0)) #Red outline if hovering over button
             else:
-                return screenButtons
+                item.draw(win,(0,0,0)) #Black outline in normal state
+            if item.isClick(pos):
+                newButtons = newScreen(item,dictionary,screenButtons)
+                return newButtons
+    return screenButtons #if nothing is clicked, the screen stays the same
 
 def buttonPlacement(screenButtons):
+    """
+    This function evenly places the buttons on the screen
+    depending on the number of buttons there are
+
+    """
+    #Calculates the amount of space that will not be taken up by buttons
     totalSpace = width - buttonWidth*len(screenButtons)
     space = totalSpace/(len(screenButtons)+1)
 
     spacerIndex = space
+    #Goes through and adjusts the x-coordinates for each button placement
     for item in screenButtons:
         item.x = spacerIndex
         item.y = buttonY
         spacerIndex += buttonWidth + space
 
 
+
+
+"""
+Here we created the text boxes and buttons that we will display on the screen, and add them to dictionaries in order to establish a mapping for the buttons.
+"""
 StartDescription = TextBox((255,255,255), text='You wake up and are confused')
-Start = Button((255,255,255), text='Get up from the bed',exist = False)
-StartBack = BackButton((255,255,255), text='Go Back to Start',exist = False)
+Start = Button((255,255,255), text='Get up from the bed')
+StartBack = BackButton((255,255,255), text='Back to Start')
+
+BedroomDescription = TextBox((255,255,255), text='The room is messy and smells funky')
+Bedroom = Button((255,255,255), text='Explore Bedroom')
+
+Diary = Button((255,255,255), text='Open up her Diary')
+DiaryDescription = TextBox((255,255,255), text='The diary appears to be locked')
+DiaryBack = BackButton((255,255,255), text='Go Back')
+
+Pillow = Button((255,255,255), text='Scream into pillow')
+PillowDescription = TextBox((255,255,255), text='The pillow is old and flat')
+PillowBack = BackButton((255,255,255), text='Go Back')
+
+Kitchen = Button((255,255,255), text='Explore Kitchen')
+KitchenDescription = TextBox((255,255,255), text='The kitchen smells weird')
+
+Fork = Button((255,255,255), text='Fork as a weapon')
+ForkDescription = TextBox((255,255,255), text='The fork is oddly sharp')
+ForkBack = BackButton((255,255,255), text='Go Back')
+
+Fridge = Button((255,255,255), text='Put head in fridge')
+FridgeDescription = TextBox((255,255,255), text='There is mold everywhere')
+FridgeBack = BackButton((255,255,255), text='Go Back')
 
 
-BedroomDescription = TextBox((255,255,255), text='The room is very messy and smells funky')
-Bedroom = Button((255,255,255), text='Explore the room')
+"""
+Screens is our initial dictionary with key and buttons/ textboxes as the values
+"""
 
-Diary = Button((255,255,255), text='Open up her Diary',exist = False)
-DiaryDescription = TextBox((255,255,255), text='The diary appears to be locked.')
-DiaryBack = BackButton((255,255,255), text='Diary - Back',exist = False)
-
-Pillow = Button((255,255,255), text='Scream into a pillow',exist = False)
-PillowDescription = TextBox((255,255,255), text='The pillow is old and flat',exist = False)
-PillowBack = BackButton((255,255,255), text='Pillow - Back',exist = False)
-
-Kitchen = Button((255,255,255), text='Kitchen',exist = False)
-KitchenDescription = TextBox((255,255,255), text='The kitchen smells weird',exist = False)
-
-Fork = Button((255,255,255), text='Use the fork as a weapon',exist = False)
-ForkDescription = TextBox((255,255,255), text='The fork is oddly sharp',exist = False)
-ForkBack = BackButton((255,255,255), text='Fork - Back',exist = False)
-
-Fridge = Button((255,255,255), text='Stick your head in the fridge',exist = False)
-FridgeDescription = TextBox((255,255,255), text='There is mold everywhere',exist = False)
-FridgeBack = BackButton((255,255,255), text='Fridge - Back',exist = False)
-
-
-
-Screens = {Start : [StartDescription, Bedroom, Kitchen], Bedroom : [StartBack, BedroomDescription,Pillow,Diary], Kitchen : [StartBack, KitchenDescription,Fork,Fridge],Pillow : [PillowDescription, PillowBack], Diary : [DiaryDescription, DiaryBack], Fork : [ForkDescription, ForkBack], Fridge : [FridgeDescription,FridgeBack], StartBack : [Kitchen,Bedroom], ForkBack : [Fridge,Fork,StartBack],FridgeBack : [Fridge,Fork,StartBack], DiaryBack: [Pillow,Diary,StartBack],PillowBack: [Pillow,Diary,StartBack]}
+Screens = {Start : [StartDescription, Bedroom, Kitchen], Bedroom : [StartBack, BedroomDescription,Pillow,Diary], Kitchen : [StartBack, KitchenDescription,Fork,Fridge],Pillow : [PillowDescription, PillowBack], Diary : [DiaryDescription, DiaryBack], Fork : [ForkDescription, ForkBack], Fridge : [FridgeDescription,FridgeBack], StartBack : [StartDescription, Bedroom, Kitchen], ForkBack : [Fridge,Fork,StartBack,KitchenDescription],FridgeBack : [Fridge,Fork,StartBack, KitchenDescription], DiaryBack: [Pillow,Diary,StartBack,BedroomDescription],PillowBack: [Pillow,Diary,StartBack, BedroomDescription]}
 
 
 
 if __name__ == '__main__':
 
-##TESTING newScreen(item,screen)
-    #
-    # a = Button((255,255,255),100,buttonY,buttonWidth,buttonHeight, text='A',exist = False)
-    # #b = Button((255,255,255),200,buttonY,buttonWidth,buttonHeight, text='B',exist = False)
-    # a = Button((255,255,255), text='A',exist = False)
-    # b = Button((255,255,255), text='B',exist = False)
-    # d = Button((255,255,255), text='D',exist = False)
-    # c = TextBox((255,255,255), text='C')
-    #
-    # ItemDictionary = {a : [b,a,c,d], b: [a]}
-    #
-    #
-    # newScreen(b)
-    # oldScreen = [a]
+##    FOR TESTING PURPOSES
+#     newScreen(item,screen)
+#
+#     a = Button((255,255,255), text='A')
+#     b = Button((255,255,255), text='B')
+#     d = Button((255,255,255), text='D')
+#     c = Button((255,255,255), text='C')
+#
+#     ItemDictionary = {a : [b,c], b: [c,d],c : [d,a], d: [a,b]}
+#
+#     newScreen(d,ItemDictionary)
+#     oldScreen = ItemDictionary[d]
 
-    # win.blit(pygame.transform.scale(pic, (width, height)), (0, 0))
-    newScreen(Start)
+
+    newScreen(Start, Screens)
     oldScreen = Screens[Start]
+
     while True:
         pygame.display.update()
-        # win.blit(pygame.transform.scale(pic, (width, height)), (0, 0))
 
         for event in pygame.event.get():
             pos = pygame.mouse.get_pos()
             pygame.display.update()
 
-            oldScreen = isAnythingClick(oldScreen)
+            Quitting()
+            oldScreen = isAnythingClick(oldScreen,Screens)
 
 
 
@@ -237,4 +317,4 @@ if __name__ == '__main__':
 
 
 
-    ##end
+#     ##end
