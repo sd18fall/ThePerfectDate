c33bdf311efa2cad9b386b8984b6bd9381eab37f
cynthiayong <cyong723@gmail.com>
2018-11-25 19:43:45 -0500

    Document for stage and mappingobject classes

---
+++ stageobject_mappings.py
@@ -0,0 +1,17 @@
+
+Bedroom = Stage('Bedroom', 'The room is messy and the lights are dim.', [DairyObject, OutfitMakeupObject], backbutton)
+
+
+Diary = Stage()
+DiaryObject = MappingObject()
+
+OutfitMakeup = Stage()
+OutfitMakeupObject = MappingObject()
+NotesObject = MappingObject()
+
+Kitchen = Stage()
+
+
+BedroomObject = MappingObject()
+
+BackButton = MappingObject()
